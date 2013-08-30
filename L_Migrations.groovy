78caab1d4a67c8899275806d4df9b75d482b849a
Ajinder <ajinder@agilemedialab.in>
2013-08-30 13:49:12 +0530

    making changes

--- L_Migrations.groovy
+++ L_Migrations.groovy
@@ -10,14 +10,18 @@ job {
         copyArtifacts('F_Migrations', '*') {
             latestSuccessful()
         }
-        shell('git diff `git log -1 | grep Merge: | cut -f 2 -d ":"` | grep "app/DoctrineMigration" || exit 0' &&
-              'sh bin/update-lp2conf.sh' &&
-              'composer.phar self-update' &&
-              'composer.phar update' &&
-              'mysql -u root testdb < testdb.sql' &&
-              'rm -rf  *.sql' &&
-              'ant schema-update' &&
-              'mysqldump -u root testdb > testdb.sql')
+        shell(
+"""
+git diff `git log -1 | grep Merge: | cut -f 2 -d ':'` | grep 'app/DoctrineMigration' || exit 0
+sh bin/update-lp2conf.sh
+composer.phar self-update
+composer.phar update
+mysql -u root testdb < testdb.sql
+rm -rf  *.sql
+ant schema-update
+mysqldump -u root testdb > testdb.sql
+"""
+        )
     }
     publishers {
         archiveArtifacts("testdb.sql")
