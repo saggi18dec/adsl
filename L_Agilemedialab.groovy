911665f503519dde37d077bf7c6bf340915c147f
Ajinder <ajinder@agilemedialab.in>
2013-08-30 12:30:07 +0530

    added job for L_Agilemedialab

---
+++ L_Agilemedialab.groovy
@@ -0,0 +1,28 @@
+job {
+    name 'L_Agilemedialab'
+    scm {
+        git('git@github.com:agilemedialab/agilemedialab.git', 'origin/master')
+    }
+    customWorkspace("/var/www/Agilemedialab")
+  label('F_job_slave')
+ steps {
+   copyArtifacts('F_Migrations', '*') {
+     latestSuccessful()
+   }
+   shell("mysql -u root testbackend < testdb.sql")
+   shell("/home/ec2-user/composer.phar self-update")
+   shell("echo codebase=Agilemedialab > param")
+   shell("rm -rf *.gz")
+   shell("rm -rf vendor")
+   shell("tar -zcf agilemedialab.gz *")
+   shell('echo $BUILD_NUMBER > AgilemedialabBuildNumber')
+ }
+ publishers {
+   archiveArtifacts("*.gz,AgilemedialabBuildNumber")
+   downstreamParameterized() {
+      trigger('build_www') {
+        propertiesFile('param')
+      }
+   }
+ }
+}
